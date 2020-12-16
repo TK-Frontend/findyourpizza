2243693fe9841145e4dce5af53390fe9a232e909
unknown <teqa@wp.pl>
2020-12-17 00:03:11 +0100

    Change width and hight of button

--- src/components/ingredients/ButtonsOfIngredients.js
+++ src/components/ingredients/ButtonsOfIngredients.js
@@ -4,8 +4,8 @@ const ButtonsOfIngredients = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  min-width: 9rem;
-  min-height: 30px;
+  width: 9rem;
+  height: 30px;
   border-radius: 5px;
   border: none;
   margin: 3px;
