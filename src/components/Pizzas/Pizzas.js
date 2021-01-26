d324e075a59ef363519000617d13f197d167be06
unknown <teqa@wp.pl>
2021-01-26 10:47:38 +0100

    Change display to grid

--- src/components/Pizzas/Pizzas.js
+++ src/components/Pizzas/Pizzas.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import pattern from '../../images/pattern.png';
-import PizzaCard from '../Pizzas/PizzaCard';
+import React from "react";
+import styled from "styled-components";
+import pattern from "../../images/pattern.png";
+import PizzaCard from "../Pizzas/PizzaCard";
+import cardLayout from "../../images/cardLayout.png";
 
 const Pizzas = () => {
   return (
     <AllPizzas>
       <PizzaCard />
+      <img src={cardLayout} alt="card" />
     </AllPizzas>
   );
 };
 
 const AllPizzas = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 2fr 2fr;
+  // grid-gap: 20px;
+
   background-image: url(${pattern});
   background-repeat: no-repeat;
   background-size: 100%;
-  height: 500px;
-  padding: 5rem;
+
+  padding: 1rem;
+
+  // border: 2px solid cadetblue;
+
+  img {
+    margin-top: -12px;
+    max-width: 85%;
+    height: auto;
+  }
 `;
 
 export default Pizzas;
