daff64027cc2eba9d32aebd8b5107aa6e0a43a4f
TeqApp <teqa@wp.pl>
2021-02-07 20:25:36 +0100

    Added grid-template-areas

--- src/components/Pizzas/PizzaCard.js
+++ src/components/Pizzas/PizzaCard.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import pizzaImage from '../../images/pizza-img.png';
 
 import backgroundPattern from '../../images/cardBgImgNoRad.png';
 
 const PizzaCard = () => {
   return (
     <Card>
-      <h1>sth</h1>
+      <img src={pizzaImage} alt='img' className='imageOfPizza' />
+      <h1 className='name'>1. Pepperoni</h1>
+      <h2 className='ing'>tomato sauce, mozzarella, pepperoni</h2>
     </Card>
   );
 };
 
 const Card = styled.div`
-  display: grid;
-  grid-template-columns: repeat(22, 1fr);
-  grid-template-rows: repeat(10, 1fr);
-
   background-image: url(${backgroundPattern});
   background-repeat: no-repeat;
   background-size: 100%;
+
   width: 484px;
   height: 220px;
   border-radius: 15px;
+
+  display: grid;
+  grid-template-columns: repeat(22, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  grid-template-areas:
+    'img img img img img img img img  . . . . . . . . . . . . . . '
+    'img img img img img img img img . name name name name name name name name name name name name .'
+    'img img img img img img img img . name name name name name name name name name name name name .'
+    'img img img img img img img img . . . . . . . . . . . . . . '
+    'img img img img img img img img . ing ing ing ing ing ing ing ing ing ing ing ing .'
+    'img img img img img img img img . ing ing ing ing ing ing ing ing ing ing ing ing .'
+    'img img img img img img img img . ing ing ing ing ing ing ing ing ing ing ing ing .'
+    'img img img img img img img img . . . . . . . . . . . . . .'
+    'img img img img img img img img . . . . . . . . . . . . . .'
+    'img img img img img img img img . . . . . . . . . . . . . .';
+
+  .imageOfPizza {
+    grid-area: img;
+    align-self: center;
+    justify-self: center;
+  }
+
+  .name {
+    grid-area: name;
+    font-family: 'Archistico';
+    color: white;
+  }
+
+  .ing {
+    grid-area: ing;
+    color: white;
+    font-size: 0.84rem;
+    font-weight: 500;
+    align-self: start;
+  }
 `;
 export default PizzaCard;
